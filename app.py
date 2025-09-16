2b15ae45039f4104c89cbaab327af803ed1712c2
Losfames <felipep21silva@gmail.com>
2025-09-16 13:58:47 -0300

    Sistema de login e logout funcionando

--- app.py
+++ app.py
@@ -2,17 +2,29 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from datetime import datetime
+from sqlalchemy import or_
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'senha_secreta123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:npg_tyo9NsGnJP3F@ep-weathered-sky-ac4h3zbt-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.unauthorized_handler
+def unauthorized():
+    return jsonify({"message": "Você precisa estar logado para acessar essa função!"}), 401
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 CORS(app)
 
-#modelagem de usuario (id, nome, senha)
+
+
+# Modelagem tabela de usuario (id, nome, senha)
 class funcionario(db.Model, UserMixin):
     __tablename__ = 'funcionario'
     idfuncionario = db.Column(db.Integer, primary_key=True)
@@ -23,6 +35,10 @@ class funcionario(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     senha = db.Column(db.String(255), nullable=False)
 
+        # Corrige o erro "No id attribute"
+    def get_id(self):
+        return str(self.idfuncionario)
+
 # Modelagem tabela de remedios
 class Remedio(db.Model):
     __tablename__ = 'remedio'
@@ -30,8 +46,33 @@ class Remedio(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     descricao = db.Column(db.String(255), nullable=True)
 
-#rota de adição de funcionarios
+@login_manager.user_loader
+def load_user(user_id):
+    return funcionario.query.get(int(user_id))
+
+# Rota de login de funcionarios
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+#login com email ou cpf
+    user = funcionario.query.filter(or_(funcionario.email == data.get("email"), funcionario.cpf == data.get("cpf"))).first()
+    
+    if user and data.get("senha") == user.senha:
+            login_user(user) # cria a sessão e cookie
+            return jsonify({"message": "Login bem-sucedido!"}), 200
+    return jsonify({"message": "Credenciais inválidas!"}), 401
+
+# Rota de logout de funcionarios
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout bem-sucedido!"}), 200
+
+# Rota de adição de funcionarios
 @app.route('/api/users/add', methods=['POST'])
+@login_required
 def add_user():
     data = request.get_json()
     if not data:
@@ -64,8 +105,10 @@ def add_user():
     db.session.commit()
     return jsonify({"messege": "Funcionario adicionado com sucesso!"}), 201
 
+
 #rota de deleção de funcionarios
 @app.route('/api/users/delete/<int:user_id>', methods=['DELETE'])
+@login_required
 def delete_user(user_id):
     # recuperar o usuário pelo ID
     user = funcionario.query.get(user_id)  # usa outro nome de variável
@@ -78,6 +121,7 @@ def delete_user(user_id):
     return jsonify({"messege": "Funcionario não encontrado!"}), 404
 
 #rota para listar funcionarios
+@login_required
 @app.route('/api/users', methods=['GET'])
 def list_users():
     funcionarios = funcionario.query.all()
@@ -95,6 +139,7 @@ def list_users():
 
 # Rota de adição de remedios
 @app.route('/api/products/add', methods=['POST'])
+@login_required
 def add_product():
     data = request.get_json()
     if not data or 'nome' not in data or not data['nome'].strip():
@@ -111,6 +156,7 @@ def add_product():
 
 # Rota de deleção de remedios
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+@login_required
 def delete_product(product_id):
     # 1 - recuperar o id do produto
     remedio = Remedio.query.get(product_id)
@@ -135,8 +181,9 @@ def get_product_details(product_id):
         })
     return jsonify({"messege": "Medicamento não encontrado!"}), 404
 
-# rota de atualizaçõa de itens
+# rota de atualização de itens
 @app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+@login_required
 def update_product(product_id):
     remedio = Remedio.query.get(product_id)
     if not remedio:
